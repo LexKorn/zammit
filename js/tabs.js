d489b7f092b1d8f51c157bd4314b12d854955533
Lex <fischer84@mail.ru>
2022-01-03 18:44:16 +0300

    add servises

---
+++ js/tabs.js
@@ -0,0 +1,34 @@
+function tabs() {
+    const tabContent = document.querySelectorAll('.tabcontent'),
+          tabItems = document.querySelectorAll('.tabheader__item');
+
+    tabContent.forEach(item => {
+        item.classList.add('hide');
+    });
+
+    function showTabContent() {
+        tabItems.forEach((item, i) => {
+            if (item.classList.contains('tabheader__item_active')) {
+                tabContent[i].classList.remove('hide');
+            } else {
+                tabContent[i].classList.add('hide');
+            }
+        });
+    }    
+    showTabContent();
+
+    function hideTabContent() {
+        tabItems.forEach(item => {
+            item.classList.remove('tabheader__item_active');
+        });
+    }
+
+    tabItems.forEach(item => {
+        item.addEventListener('click', () => {
+            hideTabContent();
+            item.classList.add('tabheader__item_active');            
+            showTabContent();            
+        });
+    });
+}
+tabs();
