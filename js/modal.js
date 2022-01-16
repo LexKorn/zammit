cfc093ca5fac39354fb6eba7daea29daa0d51b10
Lex <fischer84@mail.ru>
2022-01-16 17:31:22 +0300

    edited projects, slider_counter, add projects

--- js/modal.js
+++ js/modal.js
@@ -7,7 +7,16 @@ function modal() {
           modalIcePalace = document.querySelector('#modal__ice'),
           modalGMII = document.querySelector('#modal__gmii'),
           modalDRVC = document.querySelector('#modal__drvc'),
-          modalMMK = document.querySelector('#modal__mmk');
+          modalMMK = document.querySelector('#modal__mmk'),
+          modalCinema = document.querySelector('#modal__cinema'),
+          modalEpsilon = document.querySelector('#modal__epsilon'),
+          modalAngara = document.querySelector('#modal__angara'),
+          modalFive = document.querySelector('#modal__five'),
+          modalGerclia = document.querySelector('#modal__gerclia'),
+          modalOlgino = document.querySelector('#modal__olgino'),
+          modalVolga = document.querySelector('#modal__volga'),
+          modalPNK = document.querySelector('#modal__pnk'),
+          modalSouth = document.querySelector('#modal__south');
     
     items.forEach(item => {
         item.addEventListener('click', (e) => {
@@ -32,8 +41,46 @@ function modal() {
                     modalMMK.style.display = "block";
                     modalMMK.classList.add('activeModal');
                     break;
+                case 'cinema':
+                    modalCinema.style.display = "block";
+                    modalCinema.classList.add('activeModal');
+                    break;
+                case 'epsilon':
+                    modalEpsilon.style.display = "block";
+                    modalEpsilon.classList.add('activeModal');
+                    break;
+                case 'angara':
+                    modalAngara.style.display = "block";
+                    modalAngara.classList.add('activeModal');
+                    break;
+                case 'five':
+                    modalFive.style.display = "block";
+                    modalFive.classList.add('activeModal');
+                    break;
+                case 'gerclia':
+                    modalGerclia.style.display = "block";
+                    modalGerclia.classList.add('activeModal');
+                    break;
+                case 'olgino':
+                    modalOlgino.style.display = "block";
+                    modalOlgino.classList.add('activeModal');
+                    break;
+                case 'volga':
+                    modalVolga.style.display = "block";
+                    modalVolga.classList.add('activeModal');
+                    break;
+                case 'pnk':
+                    modalPNK.style.display = "block";
+                    modalPNK.classList.add('activeModal');
+                    break;
+                case 'south':
+                    modalSouth.style.display = "block";
+                    modalSouth.classList.add('activeModal');
+                    break;
             } 
+        showSlideCounter();
         slider();
+        document.body.style.overflow = 'hidden';
         });        
     }); 
 
@@ -41,6 +88,7 @@ function modal() {
         modalWindows.forEach(item => {
             item.style.display = "none";
             item.classList.remove('activeModal');
+            document.body.style.overflow = '';
         });   
     }
 
@@ -59,6 +107,23 @@ function modal() {
             }  
         });              
     });
+
+    function showSlideCounter() {
+        const activeModal = document.querySelector('.activeModal'),
+              sliderCounter = activeModal.querySelector('.offer__slider-counter');
+
+        sliderCounter.innerHTML = `
+            <div class="offer__slider-prev">
+                <img src="../icons/left.svg" alt="prev">
+            </div>
+            <span id="current">01</span>
+            /
+            <span id="total">04</span>
+            <div class="offer__slider-next">
+                <img src="../icons/right.svg" alt="next">
+            </div>
+        `;
+    }
 
     function slider() {
         const activeModal = document.querySelector('.activeModal'),
